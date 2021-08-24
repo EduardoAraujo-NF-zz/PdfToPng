bdc86f3aa96a3715ad347bb0a94c18e078778848
Eduardo Araujo <62581228+Locky-Araujo@users.noreply.github.com>
2021-08-23 23:01:52 -0300

    Update PdfsSelect.py

--- PdfsSelect.py
+++ PdfsSelect.py
@@ -1,24 +1,15 @@
 import sys
 import os
-from PyPDF2 import PdfFileReader, PdfFileMerger
+from pdf2image import convert_from_path
 
-# Salva cada argumento do comando em uma lista
-# Argumento[0] se trata do comando
-
-# Contagem de numero de argumentos
-# Visivel apenas no console para debug
 if __name__ == "__main__":
-    print(f"Arguments count: {len(sys.argv)}")
     for i, arg in enumerate(sys.argv[1:]):
-        print(f"Argument {i:>6}: {arg}")
+        print(f"Extraindo imagens do arquivo: {arg}")
 
-# captura diretorio atual
-files_dir = "./"
+pages = convert_from_path(sys.argv[1], 300)
 
-
-merger = PdfFileMerger()
-for filename in sys.argv[1:]:
-    merger.append(PdfFileReader(os.path.join(files_dir, filename)))
-
-# Salva em um arquivo chamado "000Fullpdfs.pdf"
-merger.write(os.path.join(files_dir, "000Fullpdfs.pdf"))
+#Saving pages in jpeg format
+i = 1
+for page in pages:
+    page.save(f'page{i}.jpg', 'JPEG')
+    i = i+1
